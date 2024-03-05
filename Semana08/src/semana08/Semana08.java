ce7b892fd8070a86ac0529135f3c795f2831a7bd
jumisingo <40045992+jumisingo@users.noreply.github.com>
2024-03-04 20:58:21 -0600

    Semana08: Libreria Swing, Java GUI, Contenedores y componentes de Swing.

---
+++ Semana08/src/semana08/Semana08.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana08;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Semana08 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+    
+}
